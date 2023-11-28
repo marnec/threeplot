a554a625f91aadfbd0e49a1a5d081196337b5d3b
Marco Necci <marco.necci@azzurrodigitale.com>
2023-11-28 15:04:20 +0100

    comment out failing test

--- test/unit/sample.test.ts
+++ test/unit/sample.test.ts
@@ -2,6 +2,6 @@ test("test pass", () => {
   expect(2 + 2).toBe(4);
 });
 
-test("test fail", () => {
-  expect(2 + 2).toBe(5);
-});
+// test("test fail", () => {
+//   expect(2 + 2).toBe(5);
+// });
