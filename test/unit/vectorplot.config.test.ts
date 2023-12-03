bf865b82edc6ecf15f231920fc252d27729bb328
marnec <marnec@ryzenbeast.localdomain>
2023-12-03 19:00:14 +0100

    swap jest with vitest

--- test/unit/vectorplot.config.test.ts
+++ test/unit/vectorplot.config.test.ts
@@ -1,173 +1,166 @@
-import {
-  LineConfig,
-  VectorPlotConfig,
-  VectorPlotPlaneConfig,
-} from "../../src/plots/vectorplot.config";
+import { LineConfig, VectorPlotConfig, VectorPlotPlaneConfig } from "../../src/plots/vectorplot.config";
 import { LineParams } from "../../dist/plots/line.config";
 import { VectorPlotDefault } from "../../src/plots/vectorplot.defaults";
+import { expect, test } from "vitest";
 
-describe("VectorPlotConfigurationParams", () => {
-  it("should set the color to the provided colorHex", () => {
-    const config = { color: 0xff0000 };
-    const params = new VectorPlotConfig(config);
-    expect(params.color).toBe(0xff0000);
-  });
-
-  it("should set the color to default it undefined passed", () => {
-    const config = {};
-    const params = new VectorPlotConfig(config);
-    expect(params.color).toBe(0x000000);
-  });
-
-  it("should create a new VectorPlotPlaneConfigParams object for xy, xz, yz when enabled singolarly", () => {
-    const config = { xy: true, xz: true, yz: true };
-    const params = new VectorPlotConfig(config);
-    expect(params.xy).toBeInstanceOf(VectorPlotPlaneConfig);
-    expect(params.xz).toBeInstanceOf(VectorPlotPlaneConfig);
-    expect(params.yz).toBeInstanceOf(VectorPlotPlaneConfig);
-  });
-
-  it("should keep xy, xz, yz as undefined if passed as undefined", () => {
-    const config = undefined;
-    const params = new VectorPlotConfig(config);
-    expect(params.xy).toBeUndefined();
-    expect(params.xz).toBeUndefined();
-    expect(params.yz).toBeUndefined();
-  });
-
-  it("should set xy, xz, and yz to undefined if they are not provided", () => {
-    const config = {};
-    const params = new VectorPlotConfig(config);
-    expect(params.xy).toBeUndefined();
-    expect(params.xz).toBeUndefined();
-    expect(params.yz).toBeUndefined();
-  });
-
-  it("should keep the angle undefined if no angle is provided", () => {
-    const config = {};
-    const params = new VectorPlotConfig(config);
-    expect(params.angle).toBeUndefined();
-  });
-
-  it("should keep the angle undefined if config undefined", () => {
-    const config = undefined;
-    const params = new VectorPlotConfig(config);
-    expect(params.angle).toBeUndefined();
-  });
-
-  it("should set the angle to the provided angle object if it exists", () => {
-    const config = { angle: { line: { type: "dashed", style: { color: 0x00ff00 } } } as LineParams };
-    const params = new VectorPlotConfig(config);
-    expect(params.angle).toEqual(new LineConfig(config.angle));
-  });
+test("should set the color to the provided colorHex", () => {
+  const config = { color: 0xff0000 };
+  const params = new VectorPlotConfig(config);
+  expect(params.color).toBe(0xff0000);
 });
 
-describe("VectorPlotPlaneConfigParams", () => {
-  it("should initialize all optional components to their default when true is passed as argument", () => {
-    const configParams = new VectorPlotPlaneConfig(true);
+test("should set the color to default it undefined passed", () => {
+  const config = {};
+  const params = new VectorPlotConfig(config);
+  expect(params.color).toBe(0x000000);
+});
 
-    expect(configParams.projection).toEqual(VectorPlotDefault.secondaryLine);
-    expect(configParams.component).toEqual(VectorPlotDefault.secondaryLine);
-    expect(configParams.projectionAngle).toEqual(VectorPlotDefault.secondaryLine);
+test("should create a new VectorPlotPlaneConfigParams object for xy, xz, yz when enabled singolarly", () => {
+  const config = { xy: true, xz: true, yz: true };
+  const params = new VectorPlotConfig(config);
+  expect(params.xy).toBeInstanceOf(VectorPlotPlaneConfig);
+  expect(params.xz).toBeInstanceOf(VectorPlotPlaneConfig);
+  expect(params.yz).toBeInstanceOf(VectorPlotPlaneConfig);
+});
+
+test("should keep xy, xz, yz as undefined if passed as undefined", () => {
+  const config = undefined;
+  const params = new VectorPlotConfig(config);
+  expect(params.xy).toBeUndefined();
+  expect(params.xz).toBeUndefined();
+  expect(params.yz).toBeUndefined();
+});
+
+test("should set xy, xz, and yz to undefined if they are not provided", () => {
+  const config = {};
+  const params = new VectorPlotConfig(config);
+  expect(params.xy).toBeUndefined();
+  expect(params.xz).toBeUndefined();
+  expect(params.yz).toBeUndefined();
+});
+
+test("should keep the angle undefined if no angle is provided", () => {
+  const config = {};
+  const params = new VectorPlotConfig(config);
+  expect(params.angle).toBeUndefined();
+});
+
+test("should keep the angle undefined if config undefined", () => {
+  const config = undefined;
+  const params = new VectorPlotConfig(config);
+  expect(params.angle).toBeUndefined();
+});
+
+test("should set the angle to the provided angle object if it exists", () => {
+  const config = { angle: { line: { type: "dashed", style: { color: 0x00ff00 } } } as LineParams };
+  const params = new VectorPlotConfig(config);
+  expect(params.angle).toEqual(new LineConfig(config.angle));
+});
+
+test("should initialize all optional components to their default when true is passed as argument", () => {
+  const configParams = new VectorPlotPlaneConfig(true);
+
+  expect(configParams.projection).toEqual(VectorPlotDefault.secondaryLine);
+  expect(configParams.component).toEqual(VectorPlotDefault.secondaryLine);
+  expect(configParams.projectionAngle).toEqual(VectorPlotDefault.secondaryLine);
+});
+
+test("should initialize all properties to their default when an object with all properties set to true is passed as argument", () => {
+  const configParams = new VectorPlotPlaneConfig({
+    projection: true,
+    component: true,
+    projectionAngle: true,
   });
 
-  it("should initialize all properties to their default when an object with all properties set to true is passed as argument", () => {
-    const configParams = new VectorPlotPlaneConfig({
-      projection: true,
-      component: true,
-      projectionAngle: true,
-    });
+  expect(configParams.projection).toEqual(VectorPlotDefault.secondaryLine);
+  expect(configParams.component).toEqual(VectorPlotDefault.secondaryLine);
+  expect(configParams.projectionAngle).toEqual(VectorPlotDefault.secondaryLine);
+});
 
-    expect(configParams.projection).toEqual(VectorPlotDefault.secondaryLine);
-    expect(configParams.component).toEqual(VectorPlotDefault.secondaryLine);
-    expect(configParams.projectionAngle).toEqual(VectorPlotDefault.secondaryLine);
+test("should initialize projection component to its default when projection is true", () => {
+  const configParams = new VectorPlotPlaneConfig({ projection: true });
+
+  expect(configParams.projection).toEqual(VectorPlotDefault.secondaryLine);
+  expect(configParams.component).toBeUndefined();
+  expect(configParams.projectionAngle).toBeUndefined();
+});
+
+test("should initialize component component to its default when component is true", () => {
+  const configParams = new VectorPlotPlaneConfig({ component: true });
+
+  expect(configParams.projection).toBeUndefined();
+  expect(configParams.component).toEqual(VectorPlotDefault.secondaryLine);
+  expect(configParams.projectionAngle).toBeUndefined();
+});
+
+test("should initialize projectionAngle compoenent to its default when projectionAngle is true", () => {
+  const configParams = new VectorPlotPlaneConfig({ projectionAngle: true });
+
+  expect(configParams.projection).toBeUndefined();
+  expect(configParams.component).toBeUndefined();
+  expect(configParams.projectionAngle).toEqual(VectorPlotDefault.secondaryLine);
+});
+
+test("should initialize all properties to undefined when an empty object is passed as argument", () => {
+  const configParams = new VectorPlotPlaneConfig({});
+
+  expect(configParams.projection).toBeUndefined();
+  expect(configParams.component).toBeUndefined();
+  expect(configParams.projectionAngle).toBeUndefined();
+});
+
+test("should initialize all properties to undefined when an object with all components set to false is passed as argument", () => {
+  const configParams = new VectorPlotPlaneConfig({
+    projection: false,
+    component: false,
+    projectionAngle: false,
   });
 
-  it("should initialize projection component to its default when projection is true", () => {
-    const configParams = new VectorPlotPlaneConfig({ projection: true });
+  expect(configParams.projection).toBeUndefined();
+  expect(configParams.component).toBeUndefined();
+  expect(configParams.projectionAngle).toBeUndefined();
+});
 
-    expect(configParams.projection).toEqual(VectorPlotDefault.secondaryLine);
-    expect(configParams.component).toBeUndefined();
-    expect(configParams.projectionAngle).toBeUndefined();
+test("should initialize integrate a partial configuration defining only the label with the line options", () => {
+  const partialConfig = { label: { text: "s" } };
+
+  const configParams = new VectorPlotPlaneConfig({
+    projection: partialConfig,
+    component: partialConfig,
+    projectionAngle: partialConfig,
   });
 
-  it("should initialize component component to its default when component is true", () => {
-    const configParams = new VectorPlotPlaneConfig({ component: true });
+  expect(configParams.projection).toEqual({ ...VectorPlotDefault.secondaryLine, ...partialConfig });
+  expect(configParams.component).toEqual({ ...VectorPlotDefault.secondaryLine, ...partialConfig });
+  expect(configParams.projectionAngle).toEqual({ ...VectorPlotDefault.secondaryLine, ...partialConfig });
+});
 
-    expect(configParams.projection).toBeUndefined();
-    expect(configParams.component).toEqual(VectorPlotDefault.secondaryLine);
-    expect(configParams.projectionAngle).toBeUndefined();
+test("for a single component, should initialize a default line if only the label is defined ", () => {
+  const partialConfig = { label: { text: "s" } };
+
+  const configParams = new VectorPlotPlaneConfig({
+    projection: partialConfig,
   });
 
-  it("should initialize projectionAngle compoenent to its default when projectionAngle is true", () => {
-    const configParams = new VectorPlotPlaneConfig({ projectionAngle: true });
+  expect(configParams.projection?.line).toEqual(VectorPlotDefault.secondaryLine.line);
+});
 
-    expect(configParams.projection).toBeUndefined();
-    expect(configParams.component).toBeUndefined();
-    expect(configParams.projectionAngle).toEqual(VectorPlotDefault.secondaryLine);
+test("for a single component, should initialize a default line if only the label is defined ", () => {
+  const partialConfig = { label: { text: "s" } };
+
+  const configParams = new VectorPlotPlaneConfig({
+    component: partialConfig,
   });
 
-  it("should initialize all properties to undefined when an empty object is passed as argument", () => {
-    const configParams = new VectorPlotPlaneConfig({});
+  expect(configParams.component?.line).toEqual(VectorPlotDefault.secondaryLine.line);
+});
 
-    expect(configParams.projection).toBeUndefined();
-    expect(configParams.component).toBeUndefined();
-    expect(configParams.projectionAngle).toBeUndefined();
+test("for a single component, should initialize a default line if only the label is defined ", () => {
+  const partialConfig = { label: { text: "s" } };
+
+  const configParams = new VectorPlotPlaneConfig({
+    projectionAngle: partialConfig,
   });
 
-  it("should initialize all properties to undefined when an object with all components set to false is passed as argument", () => {
-    const configParams = new VectorPlotPlaneConfig({
-      projection: false,
-      component: false,
-      projectionAngle: false,
-    });
-
-    expect(configParams.projection).toBeUndefined();
-    expect(configParams.component).toBeUndefined();
-    expect(configParams.projectionAngle).toBeUndefined();
-  });
-
-  it("should initialize integrate a partial configuration defining only the label with the line options", () => {
-    const partialConfig = { label: { text: "s" } };
-
-    const configParams = new VectorPlotPlaneConfig({
-      projection: partialConfig,
-      component: partialConfig,
-      projectionAngle: partialConfig,
-    });
-
-    expect(configParams.projection).toEqual({ ...VectorPlotDefault.secondaryLine, ...partialConfig });
-    expect(configParams.component).toEqual({ ...VectorPlotDefault.secondaryLine, ...partialConfig });
-    expect(configParams.projectionAngle).toEqual({ ...VectorPlotDefault.secondaryLine, ...partialConfig });
-  });
-
-  it("for a single component, should initialize a default line if only the label is defined ", () => {
-    const partialConfig = { label: { text: "s" } };
-
-    const configParams = new VectorPlotPlaneConfig({
-      projection: partialConfig,
-    });
-
-    expect(configParams.projection?.line).toEqual(VectorPlotDefault.secondaryLine.line);
-  });
-
-  it("for a single component, should initialize a default line if only the label is defined ", () => {
-    const partialConfig = { label: { text: "s" } };
-
-    const configParams = new VectorPlotPlaneConfig({
-      component: partialConfig,
-    });
-
-    expect(configParams.component?.line).toEqual(VectorPlotDefault.secondaryLine.line);
-  });
-
-  it("for a single component, should initialize a default line if only the label is defined ", () => {
-    const partialConfig = { label: { text: "s" } };
-
-    const configParams = new VectorPlotPlaneConfig({
-      projectionAngle: partialConfig,
-    });
-
-    expect(configParams.projectionAngle?.line).toEqual(VectorPlotDefault.secondaryLine.line);
-  });
+  expect(configParams.projectionAngle?.line).toEqual(VectorPlotDefault.secondaryLine.line);
 });
--- test/unit/vectorplot.test.ts
+++ test/unit/vectorplot.test.ts
@@ -1,122 +1,117 @@
-import {
-  ArrowHelper,
-  LineBasicMaterial,
-  Object3D,
-  Vector3
-} from "three";
+import { ArrowHelper, LineBasicMaterial, Object3D, Vector3 } from "three";
+
+import { expect, test } from "vitest";
 import { VectorPlot } from "../../src/plots/vectorplot";
 
-describe("VectorPlot", () => {
-  it("should create a VectorPlot object with valid input parameters", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(1, 1, 1);
-    const vectorPlot = new VectorPlot(origin, target);
+test("should create a VectorPlot object with valid input parameters", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(1, 1, 1);
+  const vectorPlot = new VectorPlot(origin, target);
 
-    expect(vectorPlot).toBeInstanceOf(VectorPlot);
+  expect(vectorPlot).toBeInstanceOf(VectorPlot);
+});
+
+test("should return an array of Object3D when getFrameable is called", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(1, 1, 1);
+  const vectorPlot = new VectorPlot(origin, target);
+  const frameable = vectorPlot.getDrawables();
+
+  expect(frameable).toBeInstanceOf(Array);
+  expect(frameable.length).toBeGreaterThan(0);
+  frameable.forEach((obj) => {
+    expect(obj).toBeInstanceOf(Object3D);
+  });
+});
+
+test("should create a vector arrow helper with correct parameters", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(1, 1, 1);
+  const vectorPlot = new VectorPlot(origin, target);
+  const arrowHelper = vectorPlot.getDrawables()[0] as ArrowHelper;
+
+  expect(arrowHelper).toBeInstanceOf(ArrowHelper);
+  expect(arrowHelper.position).toEqual(origin);
+  expect(arrowHelper.line.material).toBeInstanceOf(LineBasicMaterial);
+});
+
+test("should create a VectorPlot object with identical origin and target", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(0, 0, 0);
+  const vectorPlot = new VectorPlot(origin, target);
+
+  expect(vectorPlot).toBeInstanceOf(VectorPlot);
+  expect(vectorPlot.getDrawables().length).toBe(1);
+});
+
+test("should create a VectorPlot object with origin and target on different planes", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(1, 1, 0);
+  const vectorPlot = new VectorPlot(origin, target);
+  expect(vectorPlot.getDrawables().length).toBe(1);
+});
+
+test("should create a VectorPlot object with origin and target on the same plane", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(1, 1, 0);
+
+  const config = {
+    xy: {
+      projection: true,
+      component: true,
+      projectionAngle: true,
+    },
+  };
+
+  const vectorPlot = new VectorPlot(origin, target, config);
+
+  expect(vectorPlot.getDrawables().length).toBe(4);
+});
+
+test("should create a VectorPlot object with target on the origin", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(0, 0, 0);
+  const vectorPlot = new VectorPlot(origin, target);
+
+  expect(vectorPlot.getDrawables().length).toBe(1);
+});
+
+test("should create a label for the main angle", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(0, 0, 0);
+  const vectorPlot = new VectorPlot(origin, target, { angle: { label: { text: "a" } } });
+
+  expect(vectorPlot.getWritables().length).toBe(1);
+});
+
+test("should create a label for the main vector", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(0, 0, 0);
+  const vectorPlot = new VectorPlot(origin, target, { label: { text: "a" } });
+
+  expect(vectorPlot.getWritables().length).toBe(1);
+});
+
+test("should create a label for each component", () => {
+  const origin = new Vector3(0, 0, 0);
+  const target = new Vector3(0, 0, 0);
+  const vectorPlot = new VectorPlot(origin, target, {
+    xy: {
+      projection: { label: { text: "a" } },
+      component: { label: { text: "a" } },
+      projectionAngle: { label: { text: "a" } },
+    },
+    xz: {
+      projection: { label: { text: "a" } },
+      component: { label: { text: "a" } },
+      projectionAngle: { label: { text: "a" } },
+    },
+    yz: {
+      projection: { label: { text: "a" } },
+      component: { label: { text: "a" } },
+      projectionAngle: { label: { text: "a" } },
+    },
   });
 
-  it("should return an array of Object3D when getFrameable is called", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(1, 1, 1);
-    const vectorPlot = new VectorPlot(origin, target);
-    const frameable = vectorPlot.getDrawables();
-
-    expect(frameable).toBeInstanceOf(Array);
-    expect(frameable.length).toBeGreaterThan(0);
-    frameable.forEach((obj) => {
-      expect(obj).toBeInstanceOf(Object3D);
-    });
-  });
-
-  it("should create a vector arrow helper with correct parameters", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(1, 1, 1);
-    const vectorPlot = new VectorPlot(origin, target);
-    const arrowHelper = vectorPlot.getDrawables()[0] as ArrowHelper;
-
-    expect(arrowHelper).toBeInstanceOf(ArrowHelper);
-    expect(arrowHelper.position).toEqual(origin);
-    expect(arrowHelper.line.material).toBeInstanceOf(LineBasicMaterial);
-  });
-
-  it("should create a VectorPlot object with identical origin and target", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(0, 0, 0);
-    const vectorPlot = new VectorPlot(origin, target);
-
-    expect(vectorPlot).toBeInstanceOf(VectorPlot);
-    expect(vectorPlot.getDrawables().length).toBe(1);
-  });
-
-  it("should create a VectorPlot object with origin and target on different planes", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(1, 1, 0);
-    const vectorPlot = new VectorPlot(origin, target);
-    expect(vectorPlot.getDrawables().length).toBe(1);
-  });
-
-  it("should create a VectorPlot object with origin and target on the same plane", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(1, 1, 0);
-
-    const config = {
-      xy: {
-        projection: true,
-        component: true,
-        projectionAngle: true,
-      },
-    };
-
-    const vectorPlot = new VectorPlot(origin, target, config);
-
-    expect(vectorPlot.getDrawables().length).toBe(4);
-  });
-
-  it("should create a VectorPlot object with target on the origin", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(0, 0, 0);
-    const vectorPlot = new VectorPlot(origin, target);
-
-    expect(vectorPlot.getDrawables().length).toBe(1);
-  });
-
-  it("should create a label for the main angle", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(0, 0, 0);
-    const vectorPlot = new VectorPlot(origin, target, { angle: { label: { text: "a" } } });
-
-    expect(vectorPlot.getWritables().length).toBe(1);
-  });
-
-  it("should create a label for the main vector", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(0, 0, 0);
-    const vectorPlot = new VectorPlot(origin, target, { label: { text: "a" } });
-
-    expect(vectorPlot.getWritables().length).toBe(1);
-  });
-
-  it("should create a label for each component", () => {
-    const origin = new Vector3(0, 0, 0);
-    const target = new Vector3(0, 0, 0);
-    const vectorPlot = new VectorPlot(origin, target, {
-      xy: {
-        projection: { label: { text: "a" } },
-        component: { label: { text: "a" } },
-        projectionAngle: { label: { text: "a" } },
-      },
-      xz: {
-        projection: { label: { text: "a" } },
-        component: { label: { text: "a" } },
-        projectionAngle: { label: { text: "a" } },
-      },
-      yz: {
-        projection: { label: { text: "a" } },
-        component: { label: { text: "a" } },
-        projectionAngle: { label: { text: "a" } },
-      },
-    });
-
-    expect(vectorPlot.getWritables().length).toBe(9);
-  });
+  expect(vectorPlot.getWritables().length).toBe(9);
 });
